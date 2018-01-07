6d2dc614dd71506d8ab48137337aa500a6b52b30
Tomasz Wojcik <Superstar@MacBook-Pro-Tomasz.local>
2018-01-07 01:09:53 +0100

    Displaying proper band saw name in the search results

--- src/components/MetalCuttingResults.js
+++ src/components/MetalCuttingResults.js
@@ -4,13 +4,23 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import {getFormValues} from 'redux-form'
+import data from '../data/data.json'
+import _ from 'lodash'
 
 class MetalCuttingResults extends Component {
+
+  renderName() {
+    const name = data.map(item => (_.filter(item.cutting, {'sort': this.props.chosenValues.sort, 'type': this.props.chosenValues.type})).length > 0 ? item.name : null).filter(item => item !== null)
+    return <h3>{name[0]}</h3>
+  }
+
   render () {
+    const { chosenValues } = this.props
     return (
         <div>
-          {console.log(this.props.chosenValues)}
-          Wyniki
+          {console.log(chosenValues.type)}
+          <h1>Zobacz propozycje wirtualnego doradcy:</h1>
+          <h2>{this.renderName()}</h2>
         </div>
     )
   }
